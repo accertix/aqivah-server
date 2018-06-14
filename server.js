746feb51f307e71712c0f2646c2a20b46bd8d6bc
Alvin Ofori <hagiosofori@gmail.com>
2018-06-14 14:16:18 +0000

    cleaned up neo4jgraphql integration

--- server.js
+++ server.js
@@ -1,40 +1,31 @@
-require("dotenv").config()
 const resolverMap = require("./schema/resolvers")
-const typeDefs = require("./schema/typeDefs")
-const { augmentSchema } = require("neo4j-graphql-js")
+const typeDefs = require("./schema/typeDefsTemp")
 const neo4j = require("neo4j-driver").v1
-const { makeExecutableSchema } = require("graphql-tools")
-// const bodyparser = require("body-parser")
-// const express = require("express")
-// const { graphqlExpress, graphiqlExpress } = require("apollo-server-express")
 const { ApolloServer } = require("apollo-server")
 
-let driver
-// const rootValue = {}
-// const server = express()
-// const PORT = 3000
+// let driver
 
-const context = (headers, secrets) => {
-	if (!driver) {
-		driver = neo4j.driver(
-			secrets.NEO4J_URI || process.env.NEO4J_URI,
-			neo4j.auth.basic(
-				secrets.NEO4J_USER || process.env.NEO4J_USER,
-				secrets.NEO4J_PASSWORD || process.env.NEO4J_PASSWORD
-			)
-		)
-	}
-	return { driver, headers }
-}
+// const context = (headers, secrets) => {
+// 	if (!driver) {
+// 		driver = neo4j.driver(
+// 			secrets.NEO4J_URI || process.env.NEO4J_URI,
+// 			neo4j.auth.basic(
+// 				secrets.NEO4J_USER || process.env.NEO4J_USER,
+// 				secrets.NEO4J_PASSWORD || process.env.NEO4J_PASSWORD
+// 			)
+// 		)
+// 	}
+// 	return { driver, headers }
+// }
 
-const schema = makeExecutableSchema({
-	typeDefs,
-	resolverMap,
-})
+// const schema = makeExecutableSchema({
+// 	typeDefs,
+// 	resolverMap,
+// })
 
-const augmentedSchema = augmentSchema(schema)
+// const augmentedSchema = augmentSchema(schema)
 
-const server = new ApolloServer({ augmentedSchema, context })
+const server = new ApolloServer({ typeDefs, resolvers })
 
 server.listen().then(({ url }) => {
 	console.log(`🚀  Server ready at ${url}`)
