78431bb40f6fea1758fd69309df4ef44999a5d88
Alvin Ofori <hagiosofori@gmail.com>
2018-06-14 13:16:41 +0000

    done w/ user model v1

---
+++ models/User.js
@@ -0,0 +1,35 @@
+module.exports = {
+    id: {
+        primary: true,
+        type: 'uuid'
+    },
+    firstname: {
+        type: 'string'
+    },
+    lastname: {
+        type: 'string'
+    },
+    phone: {
+        type: 'string',
+        unique: true
+    },
+    email: {
+        type: 'string',
+        unique: true
+    },
+    likes: {
+        type: "relationship",
+        relationship: 'LIKED',
+        direction: 'OUT',
+        target: 'Property',
+        eager: true
+        //TODO: find out about pagination.
+        //TODO: how do you fetch items which aren't eagearly loaded?
+    },
+    views: {
+        type: "relationship",
+        relationship: 'VIEWED',
+        direction: 'OUT',
+        target: 'Property'
+    }
+}
