854372f6ab43327931a26449d032c1cded1f693a
Alvin Ofori <hagiosofori@gmail.com>
2018-06-14 13:20:20 +0000

    done w/ propertyHistory v1 model

---
+++ models/PropertyHistory.js
@@ -0,0 +1,18 @@
+module.exports = {
+    id: {
+        type: 'uuid',
+        primary: true
+    },
+    timestamp: {
+        type: 'string' //TODO: is there a way to represent date-time in neode model?
+    },
+    fieldChanged: {
+        type: 'string'
+    },
+    oldValue: {
+        type: 'string'
+    },
+    newValue: {
+        type: 'string'
+    }
+}
