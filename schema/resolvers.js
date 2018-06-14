dd5a2ba009fb524a695329f383ea3d1e4c9c288b
Alvin Ofori <hagiosofori@gmail.com>
2018-06-14 17:42:40 +0000

    done with resolvers v1

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,4 +1,5 @@
 const Neode = require("neode")
+//TODO: find out if JS array destructuring can help with easily returning accurate values
 
 const instance = new Neode.fromEnv()
 instance.withDirectory(__dirname + "/../models")
@@ -7,41 +8,51 @@ const resolvers = {
 	Query: {
 		Properties: (first, offset, limit) => {
 			return instance.all("Property").then(results => {
-				console.log(results)
-				return results
+				console.log(results._values)
+				const properties = results._values.map(each => {
+					console.log(each._node.properties)
+					return each._node.properties
+				})
+				return properties
 			})
-        },
-        Property: (id) => {
-            return instance.findById('Property', id).then((property) => {
-                console.log(property)
-                return property
-            })
-        },
-        User: (id) => {
-            return instance.findById('User', id).then((user) => {
-                console.log(user)
-                return user
-            })
-        },
-        Users: () => {
-            return instance.all('User').then((users) => {
-                console.log(users)
-                return users
-            })
-        },
-        Location: (id) => {
-            return instance.findById('Location', id).then((location) => {
-                console.log(location)
-                return location
-            })
-        },
-        Locations: () => {
-            return instance.all('Location').then((locations) => {
-                console.log(locations)
-                return locations
-            })
-        }
-	}
+		},
+		Property: $id => {
+            console.log($id)
+			return instance
+				.cypher("MATCH (p: Property) RETURN p")
+				.then(result => {
+					const prop = result.records.map((each) => {
+                        return (each._fields[0].properties)
+                    })
+                    console.log(prop)
+                    return prop[1]
+				})
+		},
+		User: id => {
+			return instance.findById("User", id).then(user => {
+				console.log(user)
+				return user
+			})
+		},
+		Users: () => {
+			return instance.all("User").then(users => {
+				console.log(users)
+				return users
+			})
+		},
+		Location: id => {
+			return instance.findById("Location", id).then(location => {
+				console.log(location)
+				return location
+			})
+		},
+		Locations: () => {
+			return instance.all("Location").then(locations => {
+				console.log(locations)
+				return locations
+			})
+		},
+	},
 }
 
 module.exports = resolvers
