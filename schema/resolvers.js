48f6737b7e5cc59f5a797a6a73a70c85a9dae486
Alvin Ofori <hagiosofori@gmail.com>
2018-06-16 10:44:25 +0000

    neode works now.

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,14 +1,14 @@
-// const Neode = require("neode")
+const Neode = require("neode")
 //TODO: find out if JS array destructuring can help with easily returning accurate values
 //TODO: figure out future of all commented out code.
-require("dotenv").config()
-const neo4j = require("neo4j-driver").v1
-const driver = neo4j.driver(
-	process.env.NEO4J_HOST,
-	neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASS)
-)
-// const instance = new Neode.fromEnv()
-// instance.withDirectory(__dirname + "/../models")
+// require("dotenv").config()
+// const neo4j = require("neo4j-driver").v1
+// const driver = neo4j.driver(
+// 	process.env.NEO4J_HOST,
+// 	neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASS)
+// )
+const instance = new Neode.fromEnv()
+instance.withDirectory(__dirname + "/../models")
 
 const resolvers = {
 	Query: {
@@ -22,7 +22,7 @@ const resolvers = {
 				return properties
 			})
 		},
-		Property: $id => {
+		Property: (_, $id) => {
 			console.log($id)
 			return instance
 				.cypher("MATCH (p: Property) WHERE p.id = {id} RETURN p", {
